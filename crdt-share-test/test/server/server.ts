135d6a54baa3f08812927306304d2272ce75538a
tomer <101682824+tomernahum@users.noreply.github.com>
2025-04-07 22:10:41 -0400

    hono

--- crdt-share-test/test/server/server.ts
+++ crdt-share-test/test/server/server.ts
@@ -1,12 +1,19 @@
 import { Server } from "socket.io"
 import { addDocOperation, getAllDocOperations } from "./db"
-// socketio
+import { Hono } from "hono"
+import { serve } from "@hono/node-server"
+import { validator } from "hono/validator"
 
 // Note: socketio supports binding to alternative packages to ws, including, eiows, µWebSockets.js  swapping in one of these may improve performance
 // can also integrate with regular http server (ie with express, hono)
 // may replace the whole thing with cloudflare durable objects for the data and workers for transporting, then we can keep the documents as one durable object and have it be close to the users.
 
-const io = new Server(3000, {
+const honoApp = new Hono()
+const httpServer = serve({
+    fetch: honoApp.fetch,
+    port: 3000,
+})
+const io = new Server(httpServer, {
     cors: {
         origin: "*",
     },
@@ -31,14 +38,19 @@ io.on("connection", (socket) => {
         // notify other clients listening to this doc
         socket.to(docId).emit("newUpdate", docId, update)
     })
-
-    // ----
-
-    socket.on(
-        "getFullUpdateList",
-        (docId: string, callback: (fullUpdateList: Uint8Array[]) => void) => {
-            const fullUpdateList = getAllDocOperations(docId)
-            callback(fullUpdateList)
-        }
-    )
 })
+
+honoApp.get(
+    "/getAllDocOperations/:docId",
+    validator("param", (value, c) => {
+        const docId = value["docId"]
+        if (!docId || typeof docId !== "string") {
+            return c.json({ error: "Invalid docId" }, 400)
+        }
+        return { docId }
+    }),
+    (c) => {
+        const { docId } = c.req.valid("param")
+        return c.json(getAllDocOperations(docId))
+    }
+)
