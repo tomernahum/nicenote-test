8ebc5c553b8e7f1fd415ac68e9a892a6b6919f54
tomer <101682824+tomernahum@users.noreply.github.com>
2025-05-08 00:49:06 -0400

    .

--- e2ee-sync-library-rewrite/src/0-provider-beta.ts
+++ e2ee-sync-library-rewrite/src/0-provider-beta.ts
@@ -2,36 +2,64 @@ import { ClientUpdate } from "./-types"
 import { createBaseYjsProvider, yjsPUpdateEncoder } from "./0-yjs-provider"
 import type { Doc as YDoc } from "yjs"
 import { getServerInterface } from "./1-server-client"
+import {
+    CryptoConfig,
+    getInsecureCryptoConfigForTesting,
+} from "./2-crypto-factory"
 
 // TYPES
 type localCrdtInterface<CRDTUpdate> = {
     applyRemoteUpdates: (updates: CRDTUpdate[]) => void
     subscribeToRemoteUpdates: (callback: (update: CRDTUpdate) => void) => void
 
-    // getChangesNotAppliedToAnotherYDoc: (
-    //     remoteDoc: ClientUpdate[]
-    // ) => CRDTUpdate[] // maybe
+    getChangesNotAppliedToAnotherDoc: (
+        remoteDocChanges: CRDTUpdate[]
+    ) => CRDTUpdate[] // maybe
     disconnect: () => void
 }
 type CRDTUpdateEncoder<CRDTUpdate> = {
     encode: (update: CRDTUpdate) => ClientUpdate
     decode: (update: ClientUpdate) => CRDTUpdate
+    // Maybe replace decode with one that takes into account rowId?
 }
 
 // ----
 
 export function createYjsSyncProvider(
     yDoc: YDoc,
-    params: {
-        remoteDocId: string
-    }
+    params: Parameters<typeof createCrdtSyncProvider>[2]
 ) {
+    const yjsProvider = createBaseYjsProvider(yDoc)
+    const adaptedYjsProvider: localCrdtInterface<{
+        type: "doc" | "awareness"
+        operation: Uint8Array
+    }> = {
+        ...yjsProvider,
+        getChangesNotAppliedToAnotherDoc: (remoteDocChanges) => {
+            const updates = yjsProvider.getChangesNotAppliedToAnotherYDoc(
+                remoteDocChanges.map((update) => update.operation)
+            )
+            return updates.map((update) => ({
+                type: "doc",
+                operation: update,
+            }))
+            // merging awareness not currently supported in here
+        },
+    }
     return createCrdtSyncProvider(
-        createBaseYjsProvider(yDoc),
+        adaptedYjsProvider,
         yjsPUpdateEncoder(),
         params
     )
 }
+export async function createExampleYjsSyncProvider(yDoc: YDoc) {
+    return createYjsSyncProvider(yDoc, {
+        remoteDocId: "test",
+        cryptoConfig: await getInsecureCryptoConfigForTesting(),
+        mergeInitialState: true,
+    })
+}
+
 // now can also create providers with same api but for any crdt (and I could make a crdt out of reducers too). Hopefully it is similar enough though
 
 export async function createCrdtSyncProvider<CRDTUpdate>(
@@ -39,6 +67,10 @@ export async function createCrdtSyncProvider<CRDTUpdate>(
     localInterfaceUpdateEncoder: CRDTUpdateEncoder<CRDTUpdate>,
     params: {
         remoteDocId: string
+        cryptoConfig: CryptoConfig
+        mergeInitialState?: boolean
+        // TODO
+        //onReconnect?: "mergeLocalStateIntoOnline" | "replaceLocalStateWithOnline"
     }
 ) {
     // THIS WOULD BE WHAT createSyncedYDocProviderDemo IS NOW
@@ -55,4 +87,55 @@ export async function createCrdtSyncProvider<CRDTUpdate>(
     console.debug("created server interface")
     await server.connect()
     console.debug("connected to server")
+
+    // initialize local CRDT: hydrate it with server updates, and if mergeInitialState is true, merge the initial state into the local CRDT
+    async function syncInitialState(applyLocalUpdatesToRemoteDoc: boolean) {
+        console.debug("syncing initial state")
+
+        // Apply remote updates to local doc
+        const remoteDocUpdates = await server.getRemoteUpdateList()
+        console.debug("got current remote doc updates")
+        const crdtUpdates = decodeWithRowIdToCrdt(remoteDocUpdates)
+        console.debug("decoded current remote doc updates")
+        localCrdtInterface.applyRemoteUpdates(crdtUpdates)
+        console.debug("applied current remote doc updates")
+
+        // merge in any differing state as an update to the remote doc
+        if (applyLocalUpdatesToRemoteDoc) {
+            const diffUpdates =
+                await localCrdtInterface.getChangesNotAppliedToAnotherDoc(
+                    decodeWithRowIdToCrdt(remoteDocUpdates)
+                )
+            server.addUpdates(encodeFromCrdt(diffUpdates))
+        }
+    }
+    await syncInitialState(params.mergeInitialState ?? true)
+
+    // subscribe to local updates and send them to the server
+    localCrdtInterface.subscribeToRemoteUpdates((update) => {
+        console.debug("local crdt update detected", update)
+        const encodedUpdates = encodeFromCrdt([update])
+        server.addUpdates(encodedUpdates)
+    })
+    console.debug("registered listener for local crdt updates")
+
+    return
+
+    function decodeToCrdt(updates: ClientUpdate[]): CRDTUpdate[] {
+        return updates.map((update) =>
+            localInterfaceUpdateEncoder.decode(update)
+        )
+    }
+    function decodeWithRowIdToCrdt(
+        updates: { update: ClientUpdate; rowId: number }[]
+    ): CRDTUpdate[] {
+        return updates.map((update) =>
+            localInterfaceUpdateEncoder.decode(update.update)
+        )
+    }
+    function encodeFromCrdt(updates: CRDTUpdate[]): ClientUpdate[] {
+        return updates.map((update) =>
+            localInterfaceUpdateEncoder.encode(update)
+        )
+    }
 }
