e9685b889ebd4ab19662cd0a65e7c8a5d6177a6e
tomer <101682824+tomernahum@users.noreply.github.com>
2025-05-26 14:49:15 -0400

    .

--- e2ee-sync-library-rewrite/src/2-crypto-factory.ts
+++ e2ee-sync-library-rewrite/src/2-crypto-factory.ts
@@ -15,22 +15,6 @@ export type CryptoConfig = {
     /** plaintext message length will be padded up to the closest of these values, in bytes. The highest value is the max supported length  of messages */
     paddingLengthCheckpoints?: number[]
 }
-/* 
-useWriteSignatures: client signs updates before encrypting them, for other clients + the server to verify
-verifyWriteSignatures: client verifies signatures of writes from other clients before applying them - this should apply for read-only clients as well
-
-old idea: separate useClientServerWriteSignatures, useClientClientWriteSignatures
-
-write signatures will likely be done with web crypto api's Ed25519 algo implementation, which should ship in chrome soon (has shipped in chrome beta 137) (has shipped in firefox+safari already):
-https://groups.google.com/a/chromium.org/g/blink-dev/c/T2kriFdjXsg/m/ZeD_PoLXBwAJ?pli=1  
-(concerns raised in that thread about it not being quantum resistant, (will switch algorithms once a better one is provided by web crypto). However this is just verifying integrity of updates, which won't remain relevant for very long (not past a snapshot/compaction), so the quantum resistance is not as important as something like the obscurity-providing encryption who's traffic could be captured and analyzed in the future. Plus for our app it is only protecting from non-writer readers, protection from non-readers is already covered (by aes-256-gcm encryption, which to my understanding is quantum resistant as far as anyone knows, as long as it is not undermined by a separate key exchange protocol). So our threat model currently does not  protect against someone who has gained read access, but not write access, and also has a powerful quantum computer before we can roll out a post-quantum algorithm and get more doc activity to invalidate the old writes
-
-server will know the public key, so it can block spam before it reaches client (will be configurable) - server already has DoS ability from blocking messages, so nothing lost
-all readers will also know the public key, so they can verify writes for themselves in case of malicious server
-
-writes will probably be done on top of already-encrypted data, though I will look into this more to make sure that is not a bad practice
-
-*/
 const DEFAULT_ENCRYPTION_CONFIG_VALUES = {
     paddingLengthCheckpoints: [256, 2048, 16_384, 65_536, 262144],
 } 
@@ -329,7 +313,26 @@ function createEncryptionLogic(config: Config) {
 }
 // see also generateSymmetricEncryptionKey function above
 
-// TODO: signing logic
+
+function createSigningLogic(config: Config) {
+
+    /* 
+    useWriteSignatures: client signs updates before encrypting them, for other clients + the server to verify
+    verifyWriteSignatures: client verifies signatures of writes from other clients before applying them - this should apply for read-only clients as well
+
+    old idea: separate useClientServerWriteSignatures, useClientClientWriteSignatures
+
+    write signatures will likely be done with web crypto api's Ed25519 algo implementation, which should ship in chrome soon (has shipped in chrome beta 137) (has shipped in firefox+safari already):
+    https://groups.google.com/a/chromium.org/g/blink-dev/c/T2kriFdjXsg/m/ZeD_PoLXBwAJ?pli=1  
+    (concerns raised in that thread about it not being quantum resistant, (will switch algorithms once a better one is provided by web crypto). However this is just verifying integrity of updates, which won't remain relevant for very long (not past a snapshot/compaction), so the quantum resistance is not as important as something like the obscurity-providing encryption who's traffic could be captured and analyzed in the future. Plus for our app it is only protecting from non-writer readers, protection from non-readers is already covered (by aes-256-gcm encryption, which to my understanding is quantum resistant as far as anyone knows, as long as it is not undermined by a separate key exchange protocol). So our threat model currently does not  protect against someone who has gained read access, but not write access, and also has a powerful quantum computer before we can roll out a post-quantum algorithm and get more doc activity to invalidate the old writes
+
+    server will know the public key, so it can block spam before it reaches client (will be configurable) - server already has DoS ability from blocking messages, so nothing lost
+    all readers will also know the public key, so they can verify writes for themselves in case of malicious server
+
+    writes will probably be done on top of already-encrypted data, though I will look into this more to make sure that is not a bad practice
+
+    */
+}
 
 function createVersionLogic(config: Config) {
     // these are kept as functions so that the config can be changed at runtime. hopefully the js engine optimizes if they are not changed lol. if not and we see a performance issue we can cache them ourselves or actually reinitialize this function when config changes
